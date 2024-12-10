1382bc5b50f6eed171c68ad3c62dedda10907d58
Maximus <perepichkamax10@gmail.com>
2024-12-10 18:44:27 +0200

    HomeWork 01

---
+++ les 12 part1.py
@@ -0,0 +1,53 @@
+# from time import perf_counter
+#
+# users = {"+11111": "Tom",
+#          "+22222": "Bob",
+#          "+33333": "Taras"}
+#
+# print(users["+11111"])
+# users["+44444"] = "Stas"
+# users["+44444"] = "Bobian"
+# print(users["+44444"])
+#
+# key = "+1111"
+# if key in users:
+#     print(users[key])
+# else:
+#     print("Нема ключа хаха")
+# print("-"*20)
+# u2 = users.get("+22222")
+# print(u2)
+# u3 = users.get("+11111")
+# print(u3)
+# u4 = users.get("+12232413245", "Хаха ключа немає")
+# print(u4)
+#
+# key = "+12312esw"
+# if key in users:
+#     del users[key]
+# print(users)
+# delteduser = users.pop("+11111")
+# print(delteduser)
+# print(users)
+# print("-"*20)
+# ulist1 = {1: "Tom", 2: "Bob"}
+# ulist2 = {3: "Tom", 4: "Bob"}
+#
+# ulist1.update(ulist2)
+# print(ulist1)
+# print(ulist2)
+# print('*'*40)
+#
+# for key, value in users.items():
+#     print(f"key + {key}, value = {value}")
+# print('*'*40)
+# for key in users.keys():
+#     print(key)
+# for value in users.values():
+#     print(value)
+#
+#
+# t
+#
+#
+#
